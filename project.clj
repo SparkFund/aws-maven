9099b56bbcecf9b83cd61bb6f4217a1807970f9a
Donald Ball <donald.ball@gmail.com>
2016-09-22 22:51:41 -0400

    Rearchitect as a leiningen project

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject sparkfund/aws-maven "5.1.0"
+  :description "Maven wagon for S3"
+  :url "http://github.com/SparkFund/aws-maven"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :source-paths ["src/clj"]
+
+  :java-source-paths ["src/java"]
+
+  :min-lein-version "2.0.0"
+
+  :dependencies [[com.amazonaws/aws-java-s3 "1.11.36"]
+                 [org.apache.maven.wagon/wagon-provider-api "2.10"]
+                 [org.clojure/clojure "1.8.0"]]
+
+  :deploy-repositories [["releases" :clojars]])
